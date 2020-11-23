05e65446bbdd107685ace2b5f866ace9a6393261
Akbar <72993200+Akbar1002@users.noreply.github.com>
2020-11-23 13:58:27 +0100

    Delete 10.Compare arrays.js

--- Week1/homework/Js-exercises/10.Compare arrays.js
+++
@@ -1,9 +0,0 @@
-const FirstArray = ["apple", 2, "orange", true];
-
-const SecondArray = ["US", "yellow", "Spain", "red", "Italy", true, "purple"];
-
-console.log('The lenght of the array is ' + FirstArray.length);
-console.log('The lenght of the array is ' + SecondArray.length);
-
-if (FirstArray.length === SecondArray.length) { console.log("They are the same!") }
-else { console.log("Two different sizes") }
